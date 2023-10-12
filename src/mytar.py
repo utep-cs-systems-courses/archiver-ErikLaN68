d052e6f7da5807e8b3faea9ce21d6cfa2370b0c6
ErikLaN68 <eriklaneave@gmail.com>
2023-10-12 12:32:26 -0700

    Working on outbound framing

--- src/mytar.py
+++ src/mytar.py
@@ -5,27 +5,46 @@ from sys import argv, exit
 
 from buf import BufferedFdWriter, BufferedFdReader, bufferedCopy
 
-byteWriter = BufferedFdWriter(1)
+# Will use \e as the end of line data line
+def addFileTerm(byteAr):
+    #checks for file term char
+    temp = ''
+    currentByte = ''
+    for num,char in enumerate(byteAr):
+        temp = temp + str(char)
+        if len(temp) == 2:
+            if temp == '\e':
+                currentByte = currentByte + '\\'
+                currentByte = currentByte + temp
+            else:
+                currentByte = currentByte + temp
+        else:
+            currentByte = currentByte + str(char)
+    return currentByte + '\e'
+                
+        
 
 if len(argv) < 2:
     print("Not a valid amount of inputs")
     exit
 
 if argv[1] == 'c':
-    print('Will create the tar file')
-    fd = os.open(argv[2], os.O_RDONLY)
-    byteReader = BufferedFdReader(fd)
-    print(byteReader.readByte())
-    bytea = bytearray(byteReader.readByte())
-    for value in bytea:
-        print()
-    while (bv := byteReader.readByte()) is not None:
-        byteWriter.writeByte(bv)
-    byteWriter.flush()
-    # bufferedCopy(byteReader, byteWriter)
-    # print('File size ' + str(os.path.getsize(argv[2])))
-    # inputString = os.read(fd,os.path.getsize(argv[2]))
-    # print(inputString)
+    
+    inputFile = os.open(argv[2], os.O_RDONLY)
+    fileContentsSize = os.path.getsize(inputFile)
+    fileContents = os.read(inputFile, fileContentsSize)
+    print("Size of file " + str(fileContentsSize))
+    fileNameEncoded = argv[2].encode()
+    encodeFileSize = fileContentsSize.to_bytes(10, 'big')
+    print("The file size as a byte area " + str(encodeFileSize))
+    print("The file size from bytes is " + str(int.from_bytes(encodeFileSize, "big")))
+    fileNameSize = len(fileNameEncoded)
+    print("The size of the file name " + str(fileNameSize))
+    #newByteArray = fileNameSize.to_bytes(2, 'big') + fileNameEncoded + fileContentsSize.to_bytes(10, 'big') + fileContents
+    newByteArray = fileNameSize.to_bytes(2, 'big') + fileNameEncoded + fileContents
+    print(newByteArray)
+    #Decodes the information and sends it to be put back to words
+    bytearray
     exit
 
 elif argv[1] == 'x':
