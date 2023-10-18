db874368c79f4f7d4aec84f3ebb5df9fbb74cac4
ErikLaN68 <eriklaneave@gmail.com>
2023-10-17 21:49:24 -0600

    Creates a arch file and will echo the contents

--- src/mytar.py
+++ src/mytar.py
@@ -3,6 +3,8 @@
 import os
 from sys import argv, exit
 
+decode = True
+
 from buf import BufferedFdWriter, BufferedFdReader, bufferedCopy
 
 # Will use \e as the end of line data line
@@ -33,22 +35,29 @@ if argv[1] == 'c':
     inputFile = os.open(argv[2], os.O_RDONLY)
     fileContentsSize = os.path.getsize(inputFile)
     fileContents = os.read(inputFile, fileContentsSize)
-    print("Size of file " + str(fileContentsSize))
+    if decode: print("Size of file " + str(fileContentsSize))
     fileNameEncoded = argv[2].encode()
     encodeFileSize = fileContentsSize.to_bytes(2, 'big')
-    print("The file size as a byte area " + str(encodeFileSize))
-    print("The file size from bytes is " + str(int.from_bytes(encodeFileSize, "big")))
+    if decode: print("The file size as a byte area " + str(encodeFileSize))
+    if decode: print("The file size from bytes is " + str(int.from_bytes(encodeFileSize, "big")))
     fileNameSize = len(fileNameEncoded)
-    print("The size of the file name " + str(fileNameSize))
-    #newByteArray = fileNameSize.to_bytes(2, 'big') + fileNameEncoded + fileContentsSize.to_bytes(10, 'big') + fileContents
-    newByteArray = fileNameSize.to_bytes(2, 'big') + fileNameEncoded + fileContents
-    print(newByteArray)
+    if decode: print("The size of the file name " + str(fileNameSize))
+    newByteArray = fileNameSize.to_bytes(1, 'big') + fileNameEncoded + fileContentsSize.to_bytes(2, 'big') + fileContents
+    #newByteArray = fileNameSize.to_bytes(2, 'big') + fileNameEncoded + fileContents
+    if decode: print(newByteArray)
     #Decodes the information and sends it to be put back to words
-    bytearray
+    outFile = os.open('arch.mytar', os.O_WRONLY | os.O_CREAT)
+
+    #Runs through the dict to get words and amounts
+    os.write(outFile,newByteArray)
     exit
 
 elif argv[1] == 'x':
     print('Will exract the files from a given mytar file')
+    inputFile = os.open(argv[2], os.O_RDONLY)
+    fileContentsSize = os.path.getsize(inputFile)
+    fileContents = os.read(inputFile, fileContentsSize)
+    print(fileContents)
     exit
 
 else:
